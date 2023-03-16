1d8ecd504d5ce02a38aca4a979a859d569dd7cfe
Zhelev92 <niki19921111@gmail.com>
2023-03-16 16:00:04 +0200

    Switch Laptops

---
+++ 08-loops.js
@@ -0,0 +1,12 @@
+const names = ["Mark", "Sarah", "Ebony"];
+
+for (let i = 0; i < names.lenght; i++) {
+    console.log(names[i]);
+}
+
+const numbers = [1, 2, 3, 4];
+
+for (let j = 0; j < numbers.lenght; j++) {
+    console.log(numbers[j]);
+}
+
